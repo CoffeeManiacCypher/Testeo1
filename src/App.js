dae9c01d34a2648ca725c6f4551726b57fbeac5a
CoffeeManiacCypher <109939128+CoffeeManiacCypher@users.noreply.github.com>
2023-11-15 22:02:40 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 import Component from './Component';
-
+  
 function App() {
   return (
     <div className="App">
+
+      <nav className="App-nav">
+
+        <url class="nav-links">
+            <a class="btn" href="#"><button>Inicio</button></a>
+            <a class="btn" href="#"><button class="bi bi-bag-check"> Catálogo</button></a>
+            <a class="btn" href="#"><button class="bi bi-info-square-fill"> Quienes somos</button></a>
+            <a class="btn" href="#"><button class="bi bi-telephone-fill"> Contactos</button></a>
+        </url>
+
+      </nav>
+
       <header className="App-header">
+        
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Preparativos de la plataforma.
@@ -15,13 +28,26 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Component />
+        />
+        
       </header>
+
+      <body className="App-body">
+
+        Cuerpo de pagina
+        <Component />
+
+      </body>
+
+      <footer className="App-footer">
+
+        Pie de pagina
+
+      </footer>
     </div>
+
   );
 }
 
 export default App;
+
